c2129ced9a0342b0ca8d22089ffe003568a883bd
VoloshinKS <voloshinks@progressagro.com>
2022-08-15 13:11:56 +0300

    Полностью реализован функционал панели администрирования

--- ChernabogApp/Pages/Administration/Index.cshtml.cs
+++ ChernabogApp/Pages/Administration/Index.cshtml.cs
@@ -24,8 +24,18 @@ namespace ChernabogApp.Pages.Administration
         [BindProperty]
         public IList<UserRolesViewModel> model { get; set; } = new List<UserRolesViewModel>();
 
+        [BindProperty]
+        public string userRole { get; set; }
+
+        [BindProperty]
+        public string userId { get; set; }
+
+        [BindProperty]
+        public string actionType { get; set; }
+
         public class UserRolesViewModel
         {
+            public string Id { get; set; }
             public string UserName { get; set; }
             public string Email { get; set; }
             public IEnumerable<string> Roles { get; set; }
@@ -34,17 +44,13 @@ namespace ChernabogApp.Pages.Administration
         public async Task<IActionResult> OnGetAsync()
         {
 
-            if (!User.Identity.IsAuthenticated)
-            {
-                return NotFound();
-            }
-
             var users = await _userManager.Users.ToListAsync();
 
             foreach (IdentityUser user in users)
             {
                 UserRolesViewModel urv = new UserRolesViewModel()
                 {
+                    Id = user.Id,
                     UserName = user.UserName,
                     Email = user.Email,
                     Roles = await _userManager.GetRolesAsync(user)
@@ -53,6 +59,34 @@ namespace ChernabogApp.Pages.Administration
                 model.Add(urv);
             }
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            IdentityUser user = await _userManager.FindByIdAsync(userId); ;
+            switch (actionType)
+            {
+                case "addRole":
+                    bool userRoleExists = await _roleManager.RoleExistsAsync(userRole);
+                    IdentityRole role;
+                    if (!userRoleExists)
+                    {
+                        role = new IdentityRole();
+                        role.Name = userRole;
+                        await _roleManager.CreateAsync(role);
+                    }
+                    await _userManager.AddToRoleAsync(user, userRole);
+                    break;
+                case "delRole":
+                    await _userManager.RemoveFromRoleAsync(user, userRole);
+                    break;
+                case "delUser":
+                    await _userManager.DeleteAsync(user);
+                    break;
+                default:
+                    break;
+            }
+            return RedirectToPage("./Index");
         }
     }
 }
