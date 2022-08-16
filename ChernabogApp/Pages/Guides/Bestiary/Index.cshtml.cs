5bcd4e23bbab063f91482b5b889d0c55e976b3e6
VoloshinKS <voloshinks@progressagro.com>
2022-08-16 14:25:13 +0300

    Добавлен поиск по Бестиарию

--- ChernabogApp/Pages/Guides/Bestiary/Index.cshtml.cs
+++ ChernabogApp/Pages/Guides/Bestiary/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ChernabogApp.Data;
 using ChernabogApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ChernabogApp.Pages.Guides.Bestiary
 {
@@ -17,20 +18,43 @@ namespace ChernabogApp.Pages.Guides.Bestiary
         public IndexModel(ChernabogApp.Data.ChernabogAppContext context)
         {
             _context = context;
+            Categories = new SelectList(context.MonsterCategory, nameof(MonsterCategory.Id), nameof(MonsterCategory.Name));
+            CurrentFilter = "";
+            SelectedCategory = null;
         }
 
         public IList<Monster> Monster { get;set; } = default!;
-        public IList<MonsterCategory> MonsterCategory { get; set; } = default!;
+        public IList<MonsterCategory> MonsterCategories { get; set; } = default!;
+        public MonsterCategory MonsterCategory { get; set; } = default!;
+        public string CurrentFilter { get; set; }
+        public int? SelectedCategory { get; set; }
+        public SelectList Categories { get; set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string searchString, int? categoryId)
         {
             if (_context.Monster != null)
             {
-                Monster = await _context.Monster.ToListAsync();
-            }
-            if (_context.MonsterCategory != null)
-            {
-                MonsterCategory = await _context.MonsterCategory.ToListAsync();
+                CurrentFilter = searchString;
+
+                SelectedCategory = categoryId;
+
+                IQueryable<Monster> monstersIQ = from m in _context.Monster
+                                                 select m;
+
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    monstersIQ = monstersIQ.Where(m => m.Name.ToUpper().Contains(searchString.ToUpper())
+                                           || m.Description.ToUpper().Contains(searchString.ToUpper()));
+                }
+                if (SelectedCategory is not null)
+                {
+                    monstersIQ = monstersIQ.Where(m => m.CategoryId.Equals(SelectedCategory));
+                }
+                Monster = await monstersIQ.ToListAsync();
+                if (_context.MonsterCategory != null)
+                {
+                    MonsterCategories = await _context.MonsterCategory.ToListAsync();
+                }
             }
         }
     }
